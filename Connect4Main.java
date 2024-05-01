8983c0ee45b0409a55ea5259d7a4ab38008d154a
plesciam <139150976+plesciam@users.noreply.github.com>
2024-05-01 19:23:33 -0400

    Add files via upload

---
+++ Connect4Main.java
@@ -0,0 +1,10 @@
+public class Connect4Main { 
+
+    public static void main(String[] args) { 
+        @SuppressWarnings("unused") 
+        Connect4Game game = new Connect4Game(); 
+        @SuppressWarnings("unused") 
+        Connect4GUI gui = new Connect4GUI(); 
+    } 
+
+} 
---
+++ Player.java
@@ -0,0 +1,13 @@
+public class Player {
+    private char piece;
+
+    public Player(char piece) {
+        this.piece = piece;
+    }
+
+    public char getPiece() {
+        return piece;
+    }
+
+    // Other methods for making moves, etc.
+}
