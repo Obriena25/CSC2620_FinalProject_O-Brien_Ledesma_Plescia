f6bea9e38363f18711171621787fbb568f2d39aa
plesciam <plesciam@merrimack.edu>
2024-05-03 12:52:21 -0400

    Changes made to the GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,1 +1,10 @@
+public class GUI {
 
+    public static void main(String[] args) {
+        @SuppressWarnings("unused")
+        GUI game = new GUI();
+        @SuppressWarnings("unused")
+        GUI gui = new GUI();
+    }
+    
+}
