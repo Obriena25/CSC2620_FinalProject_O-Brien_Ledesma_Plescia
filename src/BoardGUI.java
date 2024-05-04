a086df0a2442cc0faf876ba1cbe3d26df8500827
plesciam <plesciam@merrimack.edu>
2024-05-04 00:50:32 -0400

    changes made to BoardGUI file

--- src/BoardGUI.java
+++ src/BoardGUI.java
@@ -1,11 +1,8 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.LayoutManager;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class BoardGUI extends JFrame {
@@ -19,7 +16,6 @@ public class BoardGUI extends JFrame {
             setSize(500, 400);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            JPanel boardPanel = new JPanel(GridLayout(ROWS, COLS));
             buttons = new JButton[ROWS][COLS];
         
             for (int i = 0; i < ROWS; i++) {
@@ -27,15 +23,11 @@ public class BoardGUI extends JFrame {
                     buttons[i][j] = new JButton();
                     buttons[i][j].setBackground(Color.WHITE);
                     buttons[i][j].setPreferredSize(new Dimension(50, 50));
-                    boardPanel.add(buttons[i][j]);
                 }
             }
-            add(boardPanel, BorderLayout.CENTER);
             setVisible(true);
         }
-        private LayoutManager GridLayout(int rOWS, int cOLS) {
-        throw new UnsupportedOperationException("Unimplemented method 'GridLayout'");
-    }
+
         public static void main(String[] args) {
             SwingUtilities.invokeLater(() -> new BoardGUI());
         }
